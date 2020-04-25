87b1f76c637f1874ac40c2bd5d6189dfd222ab01
karan preet singh sasan <karanpreet_singh@intuit.com>
2020-04-26 03:26:44 +0530

    Adding some comments

--- src/main/java/org/sasanlabs/internal/utility/annotations/ResponseType.java
+++ src/main/java/org/sasanlabs/internal/utility/annotations/ResponseType.java
@@ -1,5 +1,17 @@
 package org.sasanlabs.internal.utility.annotations;
 
+/**
+ * Usage of this is to distinguish what is the response type from the VulnerableRestEndpoint.
+ * Actually we want backend to provide entire information to frontend so that frontend 
+ * is not tightly coupled with backend. This is done with an intent that the backend can
+ * be consumer by any application without the use of frontend like say a CTF hosting 
+ * platform need not to use the UserInterface provided by vulnerableApp.
+ * 
+ * So this information will be returned with the response of /allEndPoints and /allEndPointsJson
+ * so that consumer can write the code as per the provided information by these endpoints.
+ * 
+ * @author KSASAN preetkaran20@gmail.com
+ */
 public enum ResponseType {
 	ENTIRE_HTML_PAGE,
 	JSON,
