b2aa3e7cd5413cd80c9daff33798fa7385b76667
karan preet singh sasan <karanpreet_singh@intuit.com>
2020-06-08 01:11:37 +0530

    Adding spotlessApply for javascript

--- src/main/resources/static/templates/XSSWithHtmlTagInjection/LEVEL/XSS.js
+++ src/main/resources/static/templates/XSSWithHtmlTagInjection/LEVEL/XSS.js
@@ -1,16 +1,21 @@
 function addingEventListenerToLoadImageButton() {
-	document.getElementById("submit").addEventListener('click',
-		function () {
-			let url = getUrlForVulnerabilityLevel();
-			doGetAjaxCall(appendResponseCallback, url + "?value=" + document.getElementById("textInput").value, false);
-		});
-};
+  document.getElementById("submit").addEventListener("click", function() {
+    let url = getUrlForVulnerabilityLevel();
+    doGetAjaxCall(
+      appendResponseCallback,
+      url + "?value=" + document.getElementById("textInput").value,
+      false
+    );
+  });
+}
 addingEventListenerToLoadImageButton();
 
 function appendResponseCallback(data) {
-	var parentContainer = document.getElementById("parentContainer");
-	parentContainer.innerHTML = data;
-	if(parentContainer.childNodes.length > 0) {
-		parentContainer.childNodes[0].classList.add(document.getElementById("fonts").value);
-	}
+  var parentContainer = document.getElementById("parentContainer");
+  parentContainer.innerHTML = data;
+  if (parentContainer.childNodes.length > 0) {
+    parentContainer.childNodes[0].classList.add(
+      document.getElementById("fonts").value
+    );
+  }
 }
