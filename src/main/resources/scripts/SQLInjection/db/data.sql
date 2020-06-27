10ade0abe6cf236558655c7f2d1c9644db49ee93
karan preet singh sasan <karanpreet_singh@intuit.com>
2020-06-28 02:28:03 +0530

    Adding some more data

--- src/main/resources/scripts/SQLInjection/db/data.sql
+++ src/main/resources/scripts/SQLInjection/db/data.sql
@@ -1,7 +1,9 @@
 -- USERS
-INSERT INTO USERS (first_name, last_name, email) VALUES ('Karan', 'Sasan', 'dummy@xyz.com');
-INSERT INTO USERS (first_name, last_name, email) VALUES ('Jayant', 'Mittal', 'dummy1@xyz.com');
-
+INSERT INTO USERS (name, email) VALUES ('Karan Sasan', 'dummy@xyz.com');
+-- Many are there
+INSERT INTO USERS (name, email) VALUES ('Jayant Mittal', 'dummy1@xyz.com');
+INSERT INTO USERS (name, email) VALUES ('Amit Kumar', 'dummy2@xyz.com');
+INSERT INTO USERS (name, email) VALUES ('Nirra Ishq', 'NirraIshq_MyLife@xyz.com');
 -- CARS
 INSERT INTO CARS (name, image) VALUES ('Audi', '/images/cars/Audi.jpg');
 INSERT INTO CARS (name, image) VALUES ('BMW', '/images/cars/BMW.jpg');
