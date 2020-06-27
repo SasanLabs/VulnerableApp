84ef1360f62d46f2baecf1639e46befb240620d0
karan preet singh sasan <karanpreet_singh@intuit.com>
2020-06-28 02:23:10 +0530

    Refining More Code

--- src/main/resources/scripts/SQLInjection/db/data.sql
+++ src/main/resources/scripts/SQLInjection/db/data.sql
@@ -3,5 +3,7 @@ INSERT INTO USERS (first_name, last_name, email) VALUES ('Karan', 'Sasan', 'dumm
 INSERT INTO USERS (first_name, last_name, email) VALUES ('Jayant', 'Mittal', 'dummy1@xyz.com');
 
 -- CARS
-INSERT INTO CARS (name, model, image) VALUES ('SKODA', 'RAPID', '/static/templates/ErrorBasedSQLInjectionVulnerability/SKODA.png');
-INSERT INTO CARS (name, model, image) VALUES ('BMW', '330i M Sport', '/static/templates/ErrorBasedSQLInjectionVulnerability/BMW.png');
+INSERT INTO CARS (name, image) VALUES ('Audi', '/images/cars/Audi.jpg');
+INSERT INTO CARS (name, image) VALUES ('BMW', '/images/cars/BMW.jpg');
+INSERT INTO CARS (name, image) VALUES ('Mercedes-Benz', '/images/cars/Mercedes-Benz.jpg');
+INSERT INTO CARS (name, image) VALUES ('SKODA', '/images/cars/SKODA.jpg');
