999f8c7d4aa6b19e68ce14f2f2ff51ed1b3c08d0
karan preet singh sasan <karanpreet_singh@intuit.com>
2020-07-14 02:43:30 +0530

    Reporting Vulnerability

---
+++ src/main/java/org/sasanlabs/internal/utility/annotations/RequestParameterLocation.java
@@ -0,0 +1,14 @@
+package org.sasanlabs.internal.utility.annotations;
+
+/**
+ * This class represents the location of the parameter in Request.
+ *
+ * @author KSASAN preetkaran20@gmail.com
+ */
+public enum RequestParameterLocation {
+    BODY,
+    HEADER,
+    COOKIE,
+    QUERY_PARAM,
+    PATH_PARAM
+}
