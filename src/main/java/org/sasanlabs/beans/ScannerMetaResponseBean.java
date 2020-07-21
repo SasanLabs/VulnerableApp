bbd88b4bb6d0e48bbc8c8c7a52e8865e3ce321fd
karan preet singh sasan <karanpreet_singh@intuit.com>
2020-07-22 01:56:48 +0530

    Metadata about the scanner endpoint

---
+++ src/main/java/org/sasanlabs/beans/ScannerMetaResponseBean.java
@@ -0,0 +1,39 @@
+package org.sasanlabs.beans;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import org.sasanlabs.internal.utility.annotations.RequestParameterLocation;
+import org.sasanlabs.vulnerability.types.VulnerabilitySubType;
+
+/**
+ * This class represents the meta data about the data provided by scanner endpoint. This is useful
+ * for scanners to map there vulnerability type names with VulnerableApp's vulnerability type names
+ * and same goes with the request parameter locations etc. This is mainly used for mapping
+ * conventions across different applications
+ *
+ * @author KSASAN preetkaran20@gmail.com
+ */
+public class ScannerMetaResponseBean {
+
+    @JsonProperty("availableVulnerabilities")
+    private List<VulnerabilitySubType> availableVulnerabilityTypes;
+
+    @JsonProperty("availableLocations")
+    private List<RequestParameterLocation> availableLocations;
+
+    public ScannerMetaResponseBean(
+            List<VulnerabilitySubType> availableVulnerabilityTypes,
+            List<RequestParameterLocation> availableLocations) {
+        super();
+        this.availableVulnerabilityTypes = availableVulnerabilityTypes;
+        this.availableLocations = availableLocations;
+    }
+
+    public List<VulnerabilitySubType> getAvailableVulnerabilityTypes() {
+        return availableVulnerabilityTypes;
+    }
+
+    public List<RequestParameterLocation> getAvailableLocations() {
+        return availableLocations;
+    }
+}
