5de1c2b699b417e2b108b3da285afb5276b039b6
karan preet singh sasan <karanpreet_singh@intuit.com>
2020-08-02 02:14:30 +0530

    UI changes for Command Injection

--- src/main/resources/static/templates/CommandInjection/LEVEL_1/CI_Level1.js
+++ src/main/resources/static/templates/CommandInjection/LEVEL_1/CI_Level1.js
@@ -1,14 +1,12 @@
 function addingEventListenerToPingButton() {
-  document
-    .getElementById("pingBtn")
-    .addEventListener("click", function() {
-      let url = getUrlForVulnerabilityLevel();
-      doGetAjaxCall(
-        pingUtilityCallback,
-        url + "?ipaddress=" + document.getElementById("ipaddress").value,
-        true
-      );
-    });
+  document.getElementById("pingBtn").addEventListener("click", function() {
+    let url = getUrlForVulnerabilityLevel();
+    doGetAjaxCall(
+      pingUtilityCallback,
+      url + "?ipaddress=" + document.getElementById("ipaddress").value,
+      true
+    );
+  });
 }
 addingEventListenerToPingButton();
 
