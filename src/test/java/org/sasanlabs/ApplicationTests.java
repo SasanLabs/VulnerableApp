a64e90b0faa1172541a2c829f575a073dbf04296
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-10-03 10:30:44 +0100

    Adding junit5 dependency in build.gradle and base test class (ApplicationTests.java) to check Spring context loading correctly.

---
+++ src/test/java/org/sasanlabs/ApplicationTests.java
@@ -0,0 +1,12 @@
+package org.sasanlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
