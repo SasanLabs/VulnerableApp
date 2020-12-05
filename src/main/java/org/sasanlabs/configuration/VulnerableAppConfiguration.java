4b61399cc562941914acf90d2650a49e5fe0a8eb
Zsolt Kulcsár <zsolt.kulcsar@student.tuke.sk>
2020-12-05 12:10:40 +0100

    test_1

--- src/main/java/org/sasanlabs/configuration/VulnerableAppConfiguration.java
+++ src/main/java/org/sasanlabs/configuration/VulnerableAppConfiguration.java
@@ -114,5 +114,4 @@ public class VulnerableAppConfiguration {
             @Qualifier("applicationDataSource") DataSource applicationDataSource) {
         return new JdbcTemplate(applicationDataSource);
     }
-
 }
