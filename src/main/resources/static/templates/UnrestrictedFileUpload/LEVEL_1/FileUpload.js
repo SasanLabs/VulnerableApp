91fd5d3d72a218ded90516987abc037d572ab9b0
Matt <danny20001102@gmail.com>
2022-02-21 21:39:48 +0800

    check if the file is less than 10mb, and reject the uploading if it is

--- src/main/resources/static/templates/UnrestrictedFileUpload/LEVEL_1/FileUpload.js
+++ src/main/resources/static/templates/UnrestrictedFileUpload/LEVEL_1/FileUpload.js
@@ -2,6 +2,16 @@ function addingEventListenerToUploadImage() {
   document.getElementById("upload").addEventListener("click", function () {
     var form = document.getElementById("file");
     var file = form.files[0];
+    if(file.size > 10485760){
+          let fileSizeMB = (file.size/1024)/1024
+          fileSizeMB = fileSizeMB.toFixed(3)
+          document.getElementById("uploaded_file_info").innerHTML ="File Size " + fileSizeMB + "MB Is Too Big! Please choose a smaller file"
+          document.getElementById("uploaded_file_info").style.color = "red";
+          return
+         }else{
+          document.getElementById("uploaded_file_info").innerHTML = ""
+          document.getElementById("uploaded_file_info").style.color = "black";
+         }
     var formData = new FormData();
     formData.append("file", file);
     let url = getUrlForVulnerabilityLevel();
