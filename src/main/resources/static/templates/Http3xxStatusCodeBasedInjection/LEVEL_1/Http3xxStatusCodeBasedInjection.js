b72199f3c58921d20137decf6561331643369fe6
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-06-04 17:20:01 +0100

    Http3 xx status code based injection vulnerability fix

--- src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_1/Http3xxStatusCodeBasedInjection.js
+++ src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_1/Http3xxStatusCodeBasedInjection.js
@@ -1,7 +1,14 @@
 function updatePlaceholderDiv() {
   let placeholderAnchorElement = document.getElementById("placeholder");
-  placeholderAnchorElement.href =
-    getUrlForVulnerabilityLevel() + "?returnTo=/VulnerableApp";
+  console.log(placeholderAnchorElement);
+  let url = window.location.href;
+  if (url.endsWith(":9090/VulnerableApp/")){
+    placeholderAnchorElement.href =
+        getUrlForVulnerabilityLevel() + "?returnTo=/VulnerableApp/";
+  } else if (url.endsWith("localhost/") || url.endsWith("localhost")){
+    placeholderAnchorElement.href =
+        getUrlForVulnerabilityLevel() + "?returnTo=/";
+  }
   placeholderAnchorElement.innerText = "Click here";
 }
 
