fdfe8998978aa20aba44d276b661744ecf0f5e46
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-06-20 15:35:03 +0100

    refactored the endsWith argument. Removed the port number

--- src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_1/Http3xxStatusCodeBasedInjection.js
+++ src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_1/Http3xxStatusCodeBasedInjection.js
@@ -1,7 +1,7 @@
 function updatePlaceholderDiv() {
   let placeholderAnchorElement = document.getElementById("placeholder");
   let url = window.location.href;
-  if (url.endsWith(":VulnerableApp/")){
+  if (url.endsWith("/VulnerableApp/")){
     placeholderAnchorElement.href =
         getUrlForVulnerabilityLevel() + "?returnTo=/VulnerableApp/";
   } else {
