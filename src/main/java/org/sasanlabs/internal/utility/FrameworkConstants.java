7a38e69485381c8708d1dacbe5e0876e5703b077
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-07-12 23:38:26 +0100

    unit tests for the Http3_xx_status_code_based_injection class

--- src/main/java/org/sasanlabs/internal/utility/FrameworkConstants.java
+++ src/main/java/org/sasanlabs/internal/utility/FrameworkConstants.java
@@ -19,6 +19,8 @@ public interface FrameworkConstants {
 
     String HTTP = "http://";
     String HTTPS = "https://";
+
+    String WWW = "www.";
     String COLON = ":";
     String SLASH = "/";
     String NEXT_LINE = "\n";
