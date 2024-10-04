38038e3a09f2f125cf7170102db4d8e23e512d19
Karan Preet Singh Sasan <kasasan@microsoft.com>
2024-10-04 13:26:44 -0700

    Trying with java 8

--- src/main/java/org/sasanlabs/configuration/VulnerableAppConfiguration.java
+++ src/main/java/org/sasanlabs/configuration/VulnerableAppConfiguration.java
@@ -157,8 +157,7 @@ public class VulnerableAppConfiguration {
                     return lookupMultipartResolver();
                 }
             }
-        }
-        ;
+        };
         return new MaxUploadSizeOverrideMultipartFilter();
     }
 }
