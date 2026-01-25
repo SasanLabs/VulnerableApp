d914e46d900927e3db9eccdb45bfcc59475db38a
Collins Otieno <otienocollins0549@gmail.com>
2026-01-25 23:07:22 +0300

    Fix multiple issues: Docker latest tag, Korean translation, secure XSS/XXE variants, JWT levels, Cryptographic failures vulnerability

---
+++ src/main/resources/static/templates/CryptographicFailures/LEVEL_1/CryptographicFailures.js
@@ -0,0 +1,33 @@
+function addingEventListenerToSubmitButton() {
+  document.getElementById("submitButton").addEventListener("click", function () {
+    let url = getUrlForVulnerabilityLevel();
+    let password = document.getElementById("password").value;
+    let data = document.getElementById("data").value;
+    let params = new URLSearchParams();
+    
+    if (password) {
+      params.append("password", password);
+    }
+    if (data) {
+      params.append("data", data);
+    }
+    
+    doGetAjaxCall(
+      appendResponseCallback,
+      url + "?" + params.toString(),
+      true
+    );
+  });
+}
+addingEventListenerToSubmitButton();
+
+function appendResponseCallback(data) {
+  let resultDiv = document.getElementById("result");
+  if (data.isValid) {
+    resultDiv.innerHTML = "<strong>Result:</strong> " + data.content;
+    resultDiv.style.color = "green";
+  } else {
+    resultDiv.innerHTML = "<strong>Result:</strong> " + data.content;
+    resultDiv.style.color = "red";
+  }
+}
