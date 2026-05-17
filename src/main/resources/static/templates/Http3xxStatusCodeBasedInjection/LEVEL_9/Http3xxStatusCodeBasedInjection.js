2c30080fbab2af57dff99c7e9d8348c561a5183e
Conrad Polewski <conrad.polewski@adelaide.edu.au>
2026-05-17 12:23:31 +0930

    Modify updatePlaceholderDiv function

--- src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_9/Http3xxStatusCodeBasedInjection.js
+++ src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_9/Http3xxStatusCodeBasedInjection.js
@@ -1,13 +1,9 @@
 function updatePlaceholderDiv() {
   let placeholderAnchorElement = document.getElementById("placeholder");
-  let url = window.location.href;
-  if (url.endsWith("/VulnerableApp/")) {
-    placeholderAnchorElement.href =
-      getUrlForVulnerabilityLevel() + "?returnTo=/VulnerableApp/templates/Phishing/fake-login.html";
-  } else {
-    placeholderAnchorElement.href =
-      getUrlForVulnerabilityLevel() + "?returnTo=/templates/Phishing/fake-login.html";
-  }
+  
+  placeholderAnchorElement.href =
+    getUrlForVulnerabilityLevel() + "?returnTo=/VulnerableApp/templates/Phishing/fake-login.html";
+
   placeholderAnchorElement.innerText = "Click here";
 }
 
