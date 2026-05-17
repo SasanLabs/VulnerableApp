85430f529c76fdb707e6e430ac4cbac5dae602a8
aashpis <37486859+aashpis@users.noreply.github.com>
2026-05-17 13:27:22 -0400

    move to configuration package

--- src/main/java/org/sasanlabs/configuration/ModuleSeeder.java
+++ src/main/java/org/sasanlabs/configuration/ModuleSeeder.java
@@ -1,6 +1,4 @@
-package org.sasanlabs.internal.utility;
-
-import org.sasanlabs.configuration.DatabaseSeeder;
+package org.sasanlabs.configuration;
 
 /**
  * Implement to seed the database for a module. ModuleSeeders are auto-discovered and called by
@@ -13,4 +11,10 @@ public interface ModuleSeeder {
 
     // Return true if module has already been seeded
     boolean isSeeded();
+
+    // Return name of table being seeded
+    String getModuleTable();
+
+    // Return name of Module that accesses seeded table
+    String getModuleName();
 }
