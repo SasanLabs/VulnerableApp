0c16bba02954911b58c2ae1a98276594ff655d94
Jhalak <setjy002@mymail.unisa.edu.au>
2026-05-22 00:29:48 +0930

    Merge develop into master

--- src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_9/Http3xxStatusCodeBasedInjection.js
+++ src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_9/Http3xxStatusCodeBasedInjection.js
@@ -1,15 +1,8 @@
 function updatePlaceholderDiv() {
   let placeholderAnchorElement = document.getElementById("placeholder");
-<<<<<<< HEAD
-
-  placeholderAnchorElement.href =
-    getUrlForVulnerabilityLevel() +
-    "?returnTo=/VulnerableApp/templates/Phishing/fake-login.html";
-=======
   
   placeholderAnchorElement.href =
     getUrlForVulnerabilityLevel() + "?returnTo=/VulnerableApp/phishing/fake-login.html";
->>>>>>> develop
 
   placeholderAnchorElement.innerText = "Click here";
 }
