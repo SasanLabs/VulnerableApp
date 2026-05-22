262fa518188297148741c16c2f8d168657643511
Sorrinva Taing <121933600+SorrinvaTaing@users.noreply.github.com>
2026-05-22 11:17:10 +0930

    Added changes for issues mentioned in the PR

--- src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_10/Http3xxStatusCodeBasedInjection.js
+++ src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_10/Http3xxStatusCodeBasedInjection.js
@@ -1,9 +1,20 @@
 function updatePlaceholderDiv() {
-  let placeholderAnchorElement = document.getElementById("placeholder");
-  placeholderAnchorElement.href =
-    getUrlForVulnerabilityLevel() +
-    "?returnTo=/VulnerableApp/phishing/fake-login.html";
-  placeholderAnchorElement.innerText = "Click here";
+  var returnTo = "/VulnerableApp/phishing/fake-login.html";
+  var url = getUrlForVulnerabilityLevel() + "?returnTo=" + returnTo;
+
+  document.getElementById("openRedirectBtn").addEventListener("click", function () {
+    document.getElementById("redirectUrlBox").textContent = returnTo;
+    document.getElementById("redirectModal").style.display = "flex";
+
+    document.getElementById("continueBtn").onclick = function () {
+      window.open(url, "_blank");
+      document.getElementById("redirectModal").style.display = "none";
+    };
+
+    document.getElementById("closeBtn").onclick = function () {
+      document.getElementById("redirectModal").style.display = "none";
+    };
+  });
 }
 
 updatePlaceholderDiv();
