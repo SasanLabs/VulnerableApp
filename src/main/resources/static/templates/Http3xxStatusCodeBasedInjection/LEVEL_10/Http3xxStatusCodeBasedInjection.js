2c694e4720d6dcf5d6a676c2189ce1cecd905ca9
Sorrinva Taing <121933600+SorrinvaTaing@users.noreply.github.com>
2026-05-22 11:28:58 +0930

    Fixed old tests for LEVEL_10

--- src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_10/Http3xxStatusCodeBasedInjection.js
+++ src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_10/Http3xxStatusCodeBasedInjection.js
@@ -2,19 +2,21 @@ function updatePlaceholderDiv() {
   var returnTo = "/VulnerableApp/phishing/fake-login.html";
   var url = getUrlForVulnerabilityLevel() + "?returnTo=" + returnTo;
 
-  document.getElementById("openRedirectBtn").addEventListener("click", function () {
-    document.getElementById("redirectUrlBox").textContent = returnTo;
-    document.getElementById("redirectModal").style.display = "flex";
+  document
+    .getElementById("openRedirectBtn")
+    .addEventListener("click", function () {
+      document.getElementById("redirectUrlBox").textContent = returnTo;
+      document.getElementById("redirectModal").style.display = "flex";
 
-    document.getElementById("continueBtn").onclick = function () {
-      window.open(url, "_blank");
-      document.getElementById("redirectModal").style.display = "none";
-    };
+      document.getElementById("continueBtn").onclick = function () {
+        window.open(url, "_blank");
+        document.getElementById("redirectModal").style.display = "none";
+      };
 
-    document.getElementById("closeBtn").onclick = function () {
-      document.getElementById("redirectModal").style.display = "none";
-    };
-  });
+      document.getElementById("closeBtn").onclick = function () {
+        document.getElementById("redirectModal").style.display = "none";
+      };
+    });
 }
 
 updatePlaceholderDiv();
