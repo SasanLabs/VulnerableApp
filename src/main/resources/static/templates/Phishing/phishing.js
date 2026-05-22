e6c838479c4431abb73c22c35356305719cb3167
Peter <uneph001@mymail.unisa.edu.au>
2026-05-22 12:55:14 +0930

    Added auto linking for URLS

--- src/main/resources/static/templates/Phishing/phishing.js
+++ src/main/resources/static/templates/Phishing/phishing.js
@@ -16,6 +16,26 @@ function initPhishing() {
     if (previewBody) previewBody.textContent = "ERROR: mailList element not found in HTML";
     return;
   }
+
+  // Function to convert URLs and email addresses in the mail body to clickable links
+  function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+  }
+
   
   var selectedMailId = localStorage.getItem("selectedMailId");
 
@@ -47,7 +67,7 @@ function initPhishing() {
       return;
     }
     previewSubject.textContent = mailContents[index].subject;
-    previewBody.textContent = mailContents[index].body;
+    previewBody.innerHTML = linkify(mailContents[index].body);
   }
 
   // Set selected button and update preview
