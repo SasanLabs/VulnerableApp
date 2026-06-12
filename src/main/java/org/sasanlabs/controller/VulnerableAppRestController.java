42b5826392abeb258813316201037318290204d7
Karan Preet Singh Sasan <preetkaran20@gmail.com>
2026-06-12 15:36:57 -0700

    Update VulnerableAppRestController.java

--- src/main/java/org/sasanlabs/controller/VulnerableAppRestController.java
+++ src/main/java/org/sasanlabs/controller/VulnerableAppRestController.java
@@ -4,22 +4,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.sasanlabs.beans.AllEndPointsResponseBean;
 import org.sasanlabs.beans.ScannerMetaResponseBean;
 import org.sasanlabs.beans.ScannerResponseBean;
 import org.sasanlabs.internal.utility.FrameworkConstants;
-import org.sasanlabs.internal.utility.GenericUtils;
 import org.sasanlabs.internal.utility.JSONSerializationUtils;
 import org.sasanlabs.internal.utility.annotations.RequestParameterLocation;
 import org.sasanlabs.service.IEndPointsInformationProvider;
 import org.sasanlabs.vulnerability.types.VulnerabilityType;
 import org.sasanlabs.vulnerableapp.facade.schema.VulnerabilityDefinition;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author KSASAN preetkaran20@gmail.com
@@ -32,8 +30,7 @@ public class VulnerableAppRestController {
 
     private int port;
 
-    public VulnerableAppRestController(
-            IEndPointsInformationProvider getAllSupportedEndPoints) {
+    public VulnerableAppRestController(IEndPointsInformationProvider getAllSupportedEndPoints) {
         this.getAllSupportedEndPoints = getAllSupportedEndPoints;
         this.port = port;
     }
@@ -123,13 +120,14 @@ public class VulnerableAppRestController {
      * @throws UnknownHostException
      */
     @RequestMapping("/sitemap.xml")
-    public String sitemapForPassiveScanners(HttpServletRequest request) throws JsonProcessingException, UnknownHostException {
+    public String sitemapForPassiveScanners(HttpServletRequest request)
+            throws JsonProcessingException, UnknownHostException {
         List<AllEndPointsResponseBean> allEndPoints = allEndPointsJsonResponse();
-           // Dynamically resolve host from the incoming request
-            String scheme = request.getScheme();             // http or https
-            String serverName = request.getServerName();     // actual hostname/IP
-            int serverPort = request.getServerPort();        // actual port
-    
+        // Dynamically resolve host from the incoming request
+        String scheme = request.getScheme(); // http or https
+        String serverName = request.getServerName(); // actual hostname/IP
+        int serverPort = request.getServerPort(); // actual port
+
         StringBuilder xmlBuilder =
                 new StringBuilder(
                         FrameworkConstants.GENERAL_XML_HEADER
