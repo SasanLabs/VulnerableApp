66948d454b0565b9e2ae8287e47a06dcc814861e
Chirag Panjwani <panjwanichirag123@gmail.com>
2026-07-08 02:50:00 +0530

    Update the session management endpoint logic and cosmetic look

--- src/main/resources/scripts/SessionManagement/db/schema.sql
+++ src/main/resources/scripts/SessionManagement/db/schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS session_users;
 
 CREATE TABLE session_users (
     id INT PRIMARY KEY,
+    firstname VARCHAR(25) NOT NULL,
+    lastname VARCHAR(25),
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL,
